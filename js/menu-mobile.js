6408996989c8081d31e1839f8f67acf895234bf8
Alaa Haddad <drupaldeveloper@live.com>
2023-09-06 09:28:56 -0500

    menu update

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -5,104 +5,127 @@
  */
 ((Drupal) => {
 
-  'use strict';
-  //////////////////////////////////////////////////////////////////////
-  // Steps when click hamburger icon.
-  // hamburgerIconIsClicked >> closeMobileMenuHandler >> closeMobileMenu
-  // hamburgerIconIsClicked >> openMobileMenuHandler >> openMobileMenu
+    'use strict';
+    //////////////////////////////////////////////////////////////////////
+    // Steps when click hamburger icon.
+    // hamburgerIconIsClicked >> closeMobileMenuHandler >> closeMobileMenu
+    // hamburgerIconIsClicked >> openMobileMenuHandler >> openMobileMenu
+    let currentWidth;
+    const hamburgerIconButtons = document.querySelectorAll('.solo-inner .navigation-responsive .mobile-nav');
+    // Get current width
+    const getCurrentWidth = () => window.innerWidth || document.documentElement
+        .clientWidth || document.body.clientWidth;
 
-  const hamburgerIconButtons = document.querySelectorAll('.solo-inner .navigation-responsive .mobile-nav');
-  // Get current width
-  const getCurrentWidth = () => window.innerWidth || document.documentElement
-    .clientWidth || document.body.clientWidth;
-  let currentWidth = getCurrentWidth();
+    const openMobileMenu = navTagId => {
+        const navigationMenubarClass = Drupal.solo.getNavigationMenubarClass(navTagId);
+        const subMenuClasses = Drupal.solo.getSubMenuClasses(navTagId);
 
-  const openMobileMenu = navTagId => {
-    const navigationMenubarClass = Drupal.solo.getNavigationMenubarClass(navTagId);
-    const subMenuClasses = Drupal.solo.getSubMenuClasses(navTagId);
+        subMenuClasses?.forEach((subMenu) => {
+            Drupal.solo.hideSubMenus(subMenu);
+            Drupal.solo.revertIcons(navTagId);
+        });
+        Drupal.solo.slideDown(navigationMenubarClass, 'flex');
 
-    subMenuClasses?.forEach((subMenu) => {
-      Drupal.solo.hideSubMenus(subMenu);
-      Drupal.solo.revertIcons(navTagId);
-    });
-    Drupal.solo.slideDown(navigationMenubarClass, 'flex');
+    };
 
-  };
+    const closeMobileMenu = navTagId => {
+        const navigationMenubarClass = Drupal.solo.getNavigationMenubarClass(navTagId);
+        const subMenuClasses = Drupal.solo.getSubMenuClasses(navTagId);
 
-  const closeMobileMenu = navTagId => {
-    const navigationMenubarClass = Drupal.solo.getNavigationMenubarClass(navTagId);
-    const subMenuClasses = Drupal.solo.getSubMenuClasses(navTagId);
+        subMenuClasses?.forEach((subMenu) => {
+            Drupal.solo.hideSubMenus(subMenu);
+            Drupal.solo.revertIcons(navTagId);
+        });
 
-    subMenuClasses?.forEach((subMenu) => {
-      Drupal.solo.hideSubMenus(subMenu);
-      Drupal.solo.revertIcons(navTagId);
-    });
+        Drupal.solo.slideUp(navigationMenubarClass, 400);
 
-    Drupal.solo.slideUp(navigationMenubarClass, 400);
+    };
 
-  };
+    const getMobileNavType = (hamburgerIcon) => {
+        const hamburgerIconChild = hamburgerIcon.children[0];
+        const navTagId = hamburgerIcon.closest('nav').id;
+        // page tempalte
+        return [hamburgerIconChild, navTagId];
+    };
 
-  const getMobileNavType = (hamburgerIcon) => {
-    const hamburgerIconChild = hamburgerIcon.children[0];
-    const navTagId = hamburgerIcon.closest('nav').id;
-// page tempalte
-    return [hamburgerIconChild, navTagId];
-  };
+    const hamburgerIconIsClicked = (hamburgerIcon) => {
+        const [hamburgerIconChild, navTagId] = getMobileNavType(hamburgerIcon);
 
-  const hamburgerIconIsClicked = (hamburgerIcon) => {
-    const [hamburgerIconChild, navTagId] = getMobileNavType(hamburgerIcon);
+        if (!hamburgerIcon.classList.contains('toggled')) {
+            hamburgerIconChild.setAttribute('aria-expanded', 'true');
+            hamburgerIconChild.setAttribute('aria-hidden', 'false');
+            hamburgerIcon.classList.add('toggled');
+            openMobileMenu(navTagId);
+        } else {
+            hamburgerIconChild.setAttribute('aria-expanded', 'false');
+            hamburgerIconChild.setAttribute('aria-hidden', 'true');
+            hamburgerIcon.classList.remove('toggled');
+            closeMobileMenu(navTagId);
+        }
+    };
 
-    if (!hamburgerIcon.classList.contains('toggled')) {
-      hamburgerIconChild.setAttribute('aria-expanded', 'true');
-      hamburgerIconChild.setAttribute('aria-hidden', 'false');
-      hamburgerIcon.classList.add('toggled');
-      openMobileMenu(navTagId);
-    } else {
-      hamburgerIconChild.setAttribute('aria-expanded', 'false');
-      hamburgerIconChild.setAttribute('aria-hidden', 'true');
-      hamburgerIcon.classList.remove('toggled');
-      closeMobileMenu(navTagId);
+    const addAriaControlToButton = (hamburgerIcon) => {
+        const [hamburgerIconChild, navTagId] = getMobileNavType(hamburgerIcon);
+        let ariaControl = document.querySelector(`#${navTagId} .navigation__responsive`).getAttribute('id');
+
+        if (currentWidth < 993) {
+            hamburgerIconChild.setAttribute('aria-controls', ariaControl);
+        } else {
+            hamburgerIconChild.removeAttribute('aria-controls');
+        }
+
+    };
+
+    // Onload tage the menubar id  and assign it to hamburger button.
+    function processHamburgerIcons(hamburgerIconButtons) {
+        hamburgerIconButtons.forEach((hamburgerIcon) => {
+            addAriaControlToButton(hamburgerIcon);
+        });
     }
-  };
 
-  const addAriaControlToButton = (hamburgerIcon) => {
-    const [hamburgerIconChild, navTagId] = getMobileNavType(hamburgerIcon);
-    let ariaControl = document.querySelector(`#${navTagId} .navigation__responsive`).getAttribute('id');
-
-    if (currentWidth < 993) {
-      hamburgerIconChild.setAttribute('aria-controls', ariaControl);
-    } else {
-      hamburgerIconChild.removeAttribute('aria-controls');
-    }
-
-  };
-
-  // Onload tage the menubar id  and assign it to hamburger button.
-  function processHamburgerIcons(hamburgerIconButtons) {
+    // Hamburger icon is clicked
     hamburgerIconButtons.forEach((hamburgerIcon) => {
-      addAriaControlToButton(hamburgerIcon);
+        hamburgerIcon.addEventListener('click', () => {
+            Drupal.solo.clickedHandler(() => {
+                hamburgerIconIsClicked(hamburgerIcon);
+            });
+        });
     });
-  }
 
+    const closeOnResize = (hamburgerIcon) => {
+        const [hamburgerIconChild, navTagId] = getMobileNavType(hamburgerIcon);
 
-  // Hamburger icon is clicked
-  hamburgerIconButtons.forEach((hamburgerIcon) => {
-    hamburgerIcon.addEventListener('click', () => {
-      Drupal.solo.clickedHandler(() => {
-        hamburgerIconIsClicked(hamburgerIcon);
-      });
-    });
-  });
+        hamburgerIconChild.setAttribute('aria-expanded', 'false');
+        hamburgerIconChild.setAttribute('aria-hidden', 'true');
+        hamburgerIcon.classList.remove('toggled');
+        closeMobileMenu(navTagId);
 
-  Drupal.behaviors.mobileMenu = {
-    attach: function(settings) {
+    };
 
-      window.addEventListener('resize', () => {
-        processHamburgerIcons(hamburgerIconButtons);
-        currentWidth = getCurrentWidth();
-      });
+    // This function is to keep the menubar open when changing the screen
+    // resolution over 992 px and it is called in resize event listner.
+    const resetMenusOnResize = () => {
+        hamburgerIconButtons?.forEach((hamburgerIconButton) => {
 
+            closeOnResize(hamburgerIconButton);
+
+        });
     }
-  };
+
+    Drupal.behaviors.mobileMenu = {
+        attach: function(settings) {
+
+            window.addEventListener('resize', () => {
+
+                processHamburgerIcons(hamburgerIconButtons);
+                currentWidth = getCurrentWidth();
+
+                if (currentWidth < 992) {
+                    resetMenusOnResize();
+                }
+            });
+
+        }
+    };
 
 })(Drupal);

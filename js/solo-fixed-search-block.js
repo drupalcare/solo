15babc68fb9fdd7cbfd91f10192c01c5293a6c78
Alaa Haddad <drupaldeveloper@live.com>
2023-10-29 23:13:46 -0500

    update

--- js/solo-fixed-search-block.js
+++ js/solo-fixed-search-block.js
@@ -6,45 +6,52 @@
 ((Drupal, once) => {
   'use strict';
 
+  const mainSideNav = document.querySelector('.primary-sidebar-menu .navigation-sidebar ul');
+  const searchBlock = document.getElementById('fixed-search-block');
+  const openSearch = document.querySelector('.search-button-open');
+  const closeSearch = document.querySelector('.search-button-close');
+
+  const setButtonAttributes = (element, expanded, hidden) => {
+    const button = element.querySelector('button');
+    button.setAttribute('aria-expanded', expanded);
+    button.setAttribute('aria-hidden', hidden);
+  }
+
+  const removeActiveClass = () => {
+    searchBlock.classList.remove('toggled');
+    setButtonAttributes(closeSearch, 'false', 'true');
+    setButtonAttributes(openSearch, 'false', 'true');
+  }
+
+  const closeSearchHandler = () => {
+    searchBlock.style.height = "0px";
+    searchBlock.addEventListener('transitionend', removeActiveClass, {
+      once: true
+    });
+  }
+
+  const openSearchHandler = () => {
+    if (mainSideNav) {
+      Drupal.solo.sideMenubarToggleNav(false);
+    }
+
+    searchBlock.classList.add('toggled');
+    setButtonAttributes(openSearch, 'true', 'false');
+    setButtonAttributes(closeSearch, 'true', 'false');
+
+    searchBlock.style.height = "auto";
+    let height = searchBlock.clientHeight + "px";
+    searchBlock.style.height = "0px";
+
+    setTimeout(() => {
+      searchBlock.style.height = height;
+    }, 0);
+  }
 
   Drupal.behaviors.soloFixedSearchBlock = {
-    attach: function(context, settings) {
-
-
-      const mainSideNav = document.querySelector('.primary-sidebar-menu .navigation-sidebar ul');
-
-      const searchBlock = document.getElementById('fixed-search-block');
-      const openSearch = document.querySelector('.search-button-open');
-      const closeSearch = document.querySelector('.search-button-close');
-
-      const removeActiveClass = () => {
-        searchBlock.classList.remove('toggled');
-      }
-
-      const closeSearchHandler = () => {
-        searchBlock.style.height = "0px";
-        searchBlock.addEventListener('transitionend', removeActiveClass, {once: true});
-      }
-
-      const openSearchHandler = () => {
-        // close sidenave if found.
-        if (mainSideNav ) {
-        Drupal.solo.sideMenubarToggleNav(false);
-        }
-
-        searchBlock.classList.add('toggled');
-        searchBlock.style.height = "auto";
-        let height = searchBlock.clientHeight + "px";
-        searchBlock.style.height = "0px";
-        setTimeout(() => {
-          searchBlock.style.height = height;
-        }, 0);
-      }
-
+    attach: function (settings) {
       closeSearch?.addEventListener('click', closeSearchHandler);
       openSearch?.addEventListener('click', openSearchHandler);
-
-
     }
   };
 
--- js/solo-popup-login-block.js
+++ js/solo-popup-login-block.js
@@ -4,39 +4,47 @@
  * https://www.drupal.org/node/3158256
  */
 ((Drupal, once) => {
-    'use strict';
+  'use strict';
 
+  const loginBlock = document.getElementById('popup-login-block');
+  const loginBlockInner = document.querySelector('.popup-login-block-inner');
+  const openBlock = document.querySelector('.login-button-open');
+  const closeBlock = document.querySelector('.login-button-close');
 
-    Drupal.behaviors.soloFixedLoginBlock = {
-        attach: function(settings) {
+  const setButtonAttributes = (element, expanded, hidden) => {
+    const button = element.querySelector('button');
+    button.setAttribute('aria-expanded', expanded);
+    button.setAttribute('aria-hidden', hidden);
+  }
 
-            const loginBlock = document.getElementById('popup-login-block');
-            const loginBlockInner = document.querySelector('.popup-login-block-inner');
-            const openBlock = document.querySelector('.login-block-button-open-inner');
-            const closeBlock = document.querySelector('.login-button-close');
+  const closeSearchHandler = () => {
+    loginBlock.style.display = 'none';
+    loginBlock.classList.remove('toggled');
+    setButtonAttributes(closeBlock, 'false', 'true');
+    setButtonAttributes(openBlock, 'false', 'true');
+  }
 
-            const closeSearchHandler = () => {
+  const openSearchHandler = () => {
+    loginBlock.style.display = "block";
+    loginBlock.classList.add('toggled');
+    setButtonAttributes(openBlock, 'true', 'false');
+    setButtonAttributes(closeBlock, 'true', 'false');
+  }
 
-                loginBlock.style.display = 'none';
-                loginBlock.classList.remove('toggled');
-            }
+  Drupal.behaviors.soloFixedLoginBlock = {
+    attach: function (settings) {
 
-            const openSearchHandler = () => {
-                loginBlock.style.display = "block";
-                loginBlock.classList.add('toggled');
+      closeBlock?.addEventListener('click', closeSearchHandler);
+      openBlock?.addEventListener('click', openSearchHandler);
 
-            }
-            closeBlock?.addEventListener('click', closeSearchHandler);
-            openBlock?.addEventListener('click', openSearchHandler);
-
-            //click any where to close any submenu.
-            document.addEventListener('click', (event) => {
-                if (event.target == loginBlock) {
-                    closeSearchHandler();
-                }
-            });
-
+      //click any where to close any submenu.
+      document.addEventListener('click', (event) => {
+        if (event.target == loginBlock) {
+          closeSearchHandler();
         }
-    };
+      });
+
+    }
+  };
 
 })(Drupal, once);

3e54438e92b33e4c7c907dc314b19be8e8241331
Alaa Haddad <drupaldeveloper@live.com>
2024-03-22 03:32:38 -0500

    Update javascript for login block

--- js/solo-popup-login-block.js
+++ js/solo-popup-login-block.js
@@ -9,47 +9,48 @@
 ((Drupal, once) => {
   'use strict';
 
-  const loginBlock = document.getElementById('popup-login-block');
-  const openBlock = document.querySelector('.login-button-open>button');
-  const closeBlock = document.querySelector('.login-button-close>button');
-
-  const toggleAriaAttributes = (isOpen) => {
-    // Set aria-expanded on the open and close buttons
-    openBlock.setAttribute('aria-expanded', isOpen.toString());
-    closeBlock.setAttribute('aria-expanded', isOpen.toString());
-
-    // Set aria-hidden on the login block
-    loginBlock.setAttribute('aria-hidden', (!isOpen).toString());
-  }
-
-  const closeBlockHandler = () => {
-    loginBlock.style.display = 'none';
-    loginBlock.classList.remove('toggled');
-    toggleAriaAttributes(false);
-  }
-
-  const openBlockHandler = () => {
-    loginBlock.style.display = "block";
-    loginBlock.classList.add('toggled');
-    toggleAriaAttributes(true);
-  }
-
   Drupal.behaviors.soloFixedLoginBlock = {
     attach: function (settings) {
-      closeBlock?.addEventListener('click', closeBlockHandler);
-      openBlock?.addEventListener('click', openBlockHandler);
+      const loginBlock = document.getElementById('popup-login-block');
+      const openBlock = document.querySelector('.login-button-open>button');
+      const closeBlock = document.querySelector('.login-button-close>button');
 
-      // Click anywhere to close the login block.
-      document.addEventListener('click', (event) => {
-        if (loginBlock) {
-          if (event.target === loginBlock) {
+      // Proceed only if loginBlock is found
+      if (loginBlock) {
+        const toggleAriaAttributes = (isOpen) => {
+          // Set aria-expanded on the open and close buttons if they exist
+          openBlock?.setAttribute('aria-expanded', isOpen.toString());
+          closeBlock?.setAttribute('aria-expanded', isOpen.toString());
+
+          // Set aria-hidden on the login block
+          loginBlock.setAttribute('aria-hidden', (!isOpen).toString());
+        }
+
+        const closeBlockHandler = () => {
+          loginBlock.style.display = 'none';
+          loginBlock.classList.remove('toggled');
+          toggleAriaAttributes(false);
+        }
+
+        const openBlockHandler = () => {
+          loginBlock.style.display = "block";
+          loginBlock.classList.add('toggled');
+          toggleAriaAttributes(true);
+        }
+
+        closeBlock?.addEventListener('click', closeBlockHandler);
+        openBlock?.addEventListener('click', openBlockHandler);
+
+        // Click anywhere to close the login block, excluding clicks within the loginBlock itself.
+        document.addEventListener('click', (event) => {
+          if (!loginBlock.contains(event.target)) {
             closeBlockHandler();
           }
-        }
-      });
-
+        }, true);
+      }
     }
   };
 
 })(Drupal, once);
+
 

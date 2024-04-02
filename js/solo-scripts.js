8f07278294681a4b92e45c300695d9502d946405
Alaa Haddad <drupaldeveloper@live.com>
2024-04-02 16:17:23 -0500

    Update aria-level in all menus

--- js/solo-scripts.js
+++ js/solo-scripts.js
@@ -66,12 +66,10 @@
       // those with a specific class and also excluding those with classes
       // that contain the word 'icon'
       let clickableElements = document.querySelectorAll(
-        'a > img:not(.field--name-user-picture img):not([class*="icon"]), ' +
-        'a > picture:not(.field--name-user-picture img):not([class*="icon"])' +
-        'a > img:not(.field--type-text-long img)' +
-        'a > img:not(.field--type-text-with-summary img)' +
-        'a > picture:not(.field--type-text-long img)' +
-        'a > picture:not(.field--type-text-with-summary img)'
+        'a > img:not(.field--name-user-picture):not([class*="icon"]),' +
+        'a > picture:not(.field--name-user-picture):not([class*="icon"]),' +
+        'a > img:not(.field--type-text-long):not(.field--type-text-with-summary),' +
+        'a > picture:not(.field--type-text-long):not(.field--type-text-with-summary)'
       );
 
       // Add a class to the parent <a> tag of each selected element

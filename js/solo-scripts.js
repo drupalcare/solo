277fe97a03d12738f667a4f6bc38ded9654b58f3
Alaa Haddad <drupaldeveloper@live.com>
2024-04-02 12:35:48 -0500

    update display type image inside text field and exclude clickable images inside text fields

--- js/solo-scripts.js
+++ js/solo-scripts.js
@@ -67,7 +67,11 @@
       // that contain the word 'icon'
       let clickableElements = document.querySelectorAll(
         'a > img:not(.field--name-user-picture img):not([class*="icon"]), ' +
-        'a > picture:not(.field--name-user-picture img):not([class*="icon"])'
+        'a > picture:not(.field--name-user-picture img):not([class*="icon"])' +
+        'a > img:not(.field--type-text-long img)' +
+        'a > img:not(.field--type-text-with-summary img)' +
+        'a > picture:not(.field--type-text-long img)' +
+        'a > picture:not(.field--type-text-with-summary img)'
       );
 
       // Add a class to the parent <a> tag of each selected element

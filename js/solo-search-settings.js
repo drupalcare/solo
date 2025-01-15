a5016b26f4bdb03b5ffbff7925c5cbed7d061d40
Alaa Haddad <drupaldeveloper@live.com>
2025-01-15 15:17:11 -0600

    Allow site owners to select which content types can be searched across the site. The chosen content types will apply to both the search form on the page and the Drupal Core Search Blocks.

---
+++ js/solo-search-settings.js
@@ -0,0 +1,53 @@
+/**
+ * @file
+ * Contains utility functions for Solo module.
+ *
+ * Filename: solo-search-settings.js
+ * Website: https://www.flashwebcenter.com
+ * Developer: Alaa Haddad https://www.alaahaddad.com.
+ */
+
+((Drupal, once) => {
+  'use strict';
+
+  Drupal.behaviors.soloSearchBlock = {
+    attach: function (context) {
+      // Fetch content types from drupalSettings.
+      const contentTypes = drupalSettings?.solo?.searchContentTypes;
+      console.log(contentTypes);
+      // Validate contentTypes: Ensure it's an object and not empty.
+      if (contentTypes && Object.keys(contentTypes).length > 0) {
+        // Select the search block form using `once` to avoid duplicate bindings.
+        const searchBlockForms = once('solo-search-block', '.solo-search-block-form', context);
+
+        searchBlockForms.forEach((searchBlockForm) => {
+          searchBlockForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            // Get the search input value.
+            const searchInput = searchBlockForm.querySelector('input[name="keys"]').value;
+
+            // Initialize URL with the search term.
+            let actionUrl = `/search/node?keys=${encodeURIComponent(searchInput)}`;
+
+            // Add content types as filters.
+            Object.entries(contentTypes).forEach(([key, value], index) => {
+              const filterKey = `f[${index}]`;
+              const filterValue = `type:${value}`;
+              actionUrl += `&${encodeURIComponent(filterKey)}=${encodeURIComponent(filterValue)}`;
+            });
+
+            // Ensure 'advanced-form=1' is appended only once.
+            if (!actionUrl.includes('advanced-form=1')) {
+              actionUrl += `&advanced-form=1`;
+            }
+
+            // Redirect to the new URL.
+            window.location.href = actionUrl;
+          });
+        });
+      }
+    },
+  };
+})(Drupal, once);
+
